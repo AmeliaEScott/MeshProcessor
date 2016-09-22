8941449932735d33557be38416ae84582f7cab36
Michael Morehead <mmorehea@mix.wvu.edu>
2016-09-22 10:46:06 -0400

    Cleaned up code

--- deciAndSmooth.py
+++ deciAndSmooth.py
@@ -1,6 +1,16 @@
 import glob # can get list of files in directory
 import subprocess # can call terminal commands
 import sys
+import math
+
+MESHLAB_SCRIPT = 'test.mlx' # Name of original meshlab script
+TEMP_SCRIPT = 'newScript.mlx' # Name of temporary meshlab script file to write
+MIN_FACES = 500 # Minimum number of faces needed to run the script
+MIN_DECIMATION = 0.3 # Minimum value of 'TargetPerc' parameter for decimation (for very large number of faces)
+DECIMATION_SCALE = 1000 # Higher values of this will result in meshes being less decimated when they have few faces
+
+def calc_decimation(numFaces):
+	return math.exp((MIN_FACES - numFaces) / DECIMATION_SCALE) + MIN_DECIMATION
 
 if len(sys.argv) <= 2:
 	print 'Include the base directory and output directory when calling this script.'
@@ -15,9 +25,9 @@ if not outputDir.endswith('/'):
 	outputDir += '/'
 
 files = glob.glob(baseDir + '*.obj')
-command = 'meshlabserver -s test.mlx -i %s -o ' + outputDir + '%s'
+command = 'meshlabserver -s ' + TEMP_SCRIPT + ' -i %s -o ' + outputDir + '%s'
 
-with open('test.mlx') as script:
+with open(MESHLAB_SCRIPT) as script:
 	scriptText = script.read().replace('<Param type="RichFloat" value=".2" name="TargetPerc"/>', '<Param type="RichFloat" value="%f" name="TargetPerc"/>')	
 	
 for fileName in files:
@@ -29,14 +39,15 @@ for fileName in files:
 			if line.startswith('f '):
 				numFaces += 1
 				
-	print 'File: ' + fileName + ', num vertices: ' + str(numFaces)
+	decimationValue = calc_decimation(numFaces)
+	print 'File: ' + fileName + ', num faces: ' + str(numFaces) + ', TargetPerc: ' + str(decimationValue)
 	shortFileName = fileName.replace(baseDir, '', 1)
 	if numFaces > 100:
-		with open('newScript.mlx', 'w+') as scriptOutput:
-			scriptOutput.write(scriptText % 7.0)
+		with open(TEMP_SCRIPT, 'w+') as scriptOutput:
+			scriptOutput.write(scriptText % decimationValue)
 			
 		newCommand = command % (fileName, shortFileName)
 		print newCommand
-		subprocess.call(newCommand)
+		subprocess.call(newCommand, shell=True)
 	else:
 		print 'Not running filters for file ' + fileName
